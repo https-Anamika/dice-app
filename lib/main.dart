b449929514bd638ddef62be6383cde05d0c4158d
https-Anamika <https-Anamika@github.com>
2025-05-29 11:05:10 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+ runApp(
+  MaterialApp(
+   home: Scaffold(
+    backgroundColor: Color.fromARGB(255, 189, 174, 214),
+     body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+                Colors.pink.shade200,
+                Colors.pink.shade50
+            ],
+            begin:Alignment.topCenter,
+            end:Alignment.bottomCenter
+           )
+        ),
+      child: const Center(
+       child: Text('Hello World!', style: TextStyle(
+        fontSize: 28.6,
+        color: Colors.white
+        ),
+       ), 
+        )
+        ),
+       ),
+  ),
+ );
+}
